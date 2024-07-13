911e63e8d30317ae50706be785da855421d2b46e
umair hassan <umairhassangondal3@gmail.com>
2024-07-13 22:40:53 +0500

    nav fixed + plans added

--- static/js/main.js
+++ static/js/main.js
@@ -24,22 +24,23 @@
     * -------------------------------------------------- */
     var clPreloader = function() {
         
-        $("html").addClass('cl-preload');
+        //$("html").addClass('cl-preload');
 
         $WIN.on('load', function() {
-
+            
+            console.log("window loaded");
             //force page scroll position to top at page refresh
             // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            }); 
+            // // will first fade out the loading animation 
+            // $("#loader").fadeOut("slow", function() {
+            //     // will fade out the whole DIV that covers the website.
+            //     $("#preloader").delay(300).fadeOut("slow");
+            // }); 
             
-            // for hero content animations 
-            $("html").removeClass('cl-preload');
-            $("html").addClass('cl-loaded');
+            // // for hero content animations 
+            // $("html").removeClass('cl-preload');
+            // $("html").addClass('cl-loaded');
         
         });
     };
